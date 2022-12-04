a5d5a9e4fecdacb9fc0c366e45006aea5d3d1f66
akurathisrinivas <50867173+akurathisrinivas@users.noreply.github.com>
2022-12-04 18:37:23 +0530

    three-apis-added-in-driver-module
    three-apis-added-in-driver-module

--- application/models/app/Driver1_model.php
+++ application/models/app/Driver1_model.php
@@ -77,16 +77,172 @@ class Driver1_model extends CI_Model {
             return $result;
       }
 
-      public function search_rides($driver_id,$vehicle_type,$sub_vehicle_type,$from_lat,$from_lng){
+      public function search_rides($driver_id,$travel_type,$vehicle_type,$sub_vehicle_type,$from_lat,$from_lng){
 
-            /* getting rides from taxi_orders status pending and time ridetime between now to 30 min */
+            /* getting rides from taxi_orders status pending and time ridetime between now to 10 min */
             /* apply filter from, to lat lngs with in the 10 km range get user details from taxi_orders */ 
 
-            $query="select booking_id,user_id,ride_time from taxi_orders where vehicle_type='$vehicle_type' and sub_vehicle_type='$sub_vehicle_type' and status='pending' and driver_status='Not Started' ";
+            $to_time=date('Y-m-d H:i:s');
+            $from_time = date('Y-m-d H:i:s', strtotime(' -240 minutes'));
+
+            $query="select tr.id as order_id,tr.booking_id,tr.user_id,tr.ride_time,u.first_name,u.last_name,u.mobile,u.average_rating,tr.from_lat,tr.from_lng,tr.from_address,tr.to_lat,tr.to_lng,tr.to_address,tr.driver_status,tr.vehicle_type,tr.sub_vehicle_type,tr.rider_id from taxi_orders tr inner join users u on u.id=tr.user_id where tr.vehicle_type='$vehicle_type' and tr.sub_vehicle_type='$sub_vehicle_type' and tr.status='pending' and tr.driver_status='not_started' and tr.rider_id = 0 and tr.ride_time between '$from_time' and '$to_time' ";
+
+            //echo $query;echo '<pre>';
             $orders=$this->db->query($query)->result_array();
 
-            return $orders;
+            $new_result=array();
+            if(!empty($orders)){
+            foreach($orders as $key=>$value){
+                  $rider_current_location=$this->db->query("select * from rider_current_location where rider_id='$driver_id' order by id desc")->row_array();
+//echo $this->db->last_query();
+                  $from_lat=$value['from_lat'];
+                  $from_lng=$value['from_lng'];
+                  $to_lat=$rider_current_location['lat'];
+                  $to_lng=$rider_current_location['lng'];
+
+                  $traval_to_lat=$value['to_lat'];;
+                  $traval_to_lng=$value['to_lng'];;
+                  $trip_distance = get_distance_time($from_lat, $to_lat, $from_lng, $to_lng);
+                  //echo $trip_distance;exit;
+                 // $orders[$key]['distance']=$trip_distance['distance'];
+                 // $orders[$key]['time_to_reach']=$trip_distance['time'];
+
+                  $minutes = explode(" ", $trip_distance['time']);
+                  if (in_array("hours", $minutes) && in_array("mins", $minutes)) {
+                      $seconds = ($minutes[0] * 3600) + ($minutes[2] * 60);
+                  } else {
+                      $seconds = $minutes[0] * 60;
+                  }
+                  $distance = explode(" ", $trip_distance['distance']);
+                  //var_dump($distance);
+                  if ($distance[1] == "m") {
+                      $km = $distance[0] / 1000;
+                  } else {
+                      $km = $distance[0];
+                  }
+
+                  $dis=number_format((float)$km, 3, '.', '');
+
+                  $orders[$key]['distance']=$dis;
+                  $orders[$key]['apporx_time_to_reach']=$trip_distance['time'];
+
+                  $travel_distance=$this->get_travel_distance($from_lat, $traval_to_lat, $from_lng, $traval_to_lng);
+
+                  $orders[$key]['travel_distance']=$travel_distance;
+                  $orders[$key]['amount_calulation']=$this->amount_calulations($travel_distance,$vehicle_type,$sub_vehicle_type,$travel_type);
+                  $radius=10;
+                  if($orders[$key]['distance'] <= $radius){
+                      $new_result[]=$orders[$key];
+                  }
+
+
+               }
+            }
+           // echo '<pre>';print_r($new_result);exit;
+           return $new_result;
 
       }
+
+      public function get_travel_distance($from_lat, $traval_to_lat, $from_lng, $traval_to_lng){
+
+
+                  $trip_distance = get_distance_time($from_lat, $traval_to_lat, $from_lng, $traval_to_lng);
+                  //echo $trip_distance;exit;
+                 // $orders[$key]['distance']=$trip_distance['distance'];
+                 // $orders[$key]['time_to_reach']=$trip_distance['time'];
+
+                  $minutes = explode(" ", $trip_distance['time']);
+                  if (in_array("hours", $minutes) && in_array("mins", $minutes)) {
+                      $seconds = ($minutes[0] * 3600) + ($minutes[2] * 60);
+                  } else {
+                      $seconds = $minutes[0] * 60;
+                  }
+                  $distance = explode(" ", $trip_distance['distance']);
+                  //var_dump($distance);
+                  if ($distance[1] == "m") {
+                      $km = $distance[0] / 1000;
+                  } else {
+                      $km = $distance[0];
+                  }
+
+                  $dis=number_format((float)$km, 3, '.', '');
+
+                  return $dis;
+      }
+
+      public function amount_calulations($km,$vehicle_type,$sub_vehicle_type,$travel_type){
+
+            $calculations= get_table_row('taxi_amount_calculations', array('travel_type' => $travel_type, 'vehicle_type' => $vehicle_type,'sub_vehicle_slug'=>$sub_vehicle_type));
+            //echo $this->db->last_query();exit;
+           // echo '<pre>';print_r($calculations);
+            $km=round($km);
+            //echo '<pre>';print_r($km);
+            if ($km > 0 && $km <= 1) {
+                $fare_per_km = $calculations['0to1'];
+            } elseif ($km > 1 && $km <= 10) {
+                $fare_per_km = $calculations['2to10'];
+            } elseif ($km > 10 && $km <= 30) {
+                $fare_per_km = $calculations['11to30'];
+            }
+            //echo '<pre>';print_r($fare_per_km);
+
+            $amount = (($fare_per_km * $km) + $calculations['base_fare']);
+
+            $amount_according_to_seats = (($fare_per_km * $km) + $calculations['base_fare']) * $seats_required;
+            $amount_per_head = (($fare_per_km * $km));
+
+            $ciao_commission = ($calculations['ciao_commission'] / 100) * $amount_according_to_seats;
+
+            //$tax = ($calculations['service_tax'] / 100) * $amount_according_to_seats;
+            $tax = ($calculations['service_tax'] / 100) * $ciao_commission;
+
+            $payment_gateway_commision = ($calculations['payment_gateway_commision'] / 100) * $amount_according_to_seats;
+
+            $total_amount = $amount_according_to_seats + $tax + $payment_gateway_commision + $ciao_commission;
+
+            /* Driver per seat calculation */
+
+            $per_seat_ciao_commission = ($calculations['ciao_commission'] / 100) * $amount;
+            $per_seat_tax = ($calculations['service_tax'] / 100) * $per_seat_ciao_commission;
+            $per_seat_payment_gateway_commision = ($calculations['payment_gateway_commision'] / 100) * $amount;
+
+            $per_seat_amount = $amount - $tax - $payment_gateway_commision - $ciao_commission;
+
+            $subCalculations=array(
+                  'travel_type'=>'car',
+                  'sub_vehicle_type'=>$calculations['sub_vehicle_type'],
+                  'max_seat_capacity'=>$calculations['seat_capacity'],
+                  'distance' => $km,
+                  'amount' => round($amount), 
+                  'tax' => round($tax), 
+                  'payment_gateway_commision' => round($payment_gateway_commision), 
+                  'ciao_commission' => round($ciao_commission), 
+                  'total_amount' => round($total_amount), 
+                  //'cancellation_charges' => (int) $cancellation_charges['cancellation_charges'],
+                  'cancellation_charges'=>0,
+                  'amount_per_head' => round($amount_per_head), 
+                  'base_fare' => (int) $calculations['base_fare'], 
+                  'per_seat_amount' => round($per_seat_amount)
+                 );
+             // echo '<pre>';print_r($subCalculations);exit;
+            $final_amount= round($subCalculations['amount']*$subCalculations['max_seat_capacity']);
+            return $final_amount;
+      }
+
+      function check_order_started($order_id) {
+        $this->db->order_by('taxi_orders.id', 'desc');
+        $this->db->where('taxi_orders.status !=', 'pending');
+        $this->db->where('taxi_orders.status !=', 'accepted');
+        //$this->db->where('orders.status !=', 'cancelled by user');
+        //$this->db->where('orders.status !=', 'cancelled by rider');
+        $this->db->where('taxi_orders.id', $order_id);
+        $this->db->select('taxi_orders.id');
+        $orders = $this->db->get('taxi_orders');
+        //echo $this->db->last_query();exit;
+        if ($orders->num_rows() > 0) {
+            return $orders->row_array();
+        }
+        return new ArrayObject();
+    }
 
 }?>
